4b3bd963919f3df4a76c27d0ad2feb8c4722095b
ouyangpeng <ouyangpeng@oaserver.dw.gdbbk.com>
2018-12-19 16:36:23 +0800

    1、调整显示的每个方块的大小

--- app/src/main/java/cn/oyp/link/utils/GameConf.java
+++ app/src/main/java/cn/oyp/link/utils/GameConf.java
@@ -11,11 +11,11 @@ public class GameConf {
     /**
      * 连连看的每个方块的图片的宽
      */
-    public static final int PIECE_WIDTH = 150;
+    public static final int PIECE_WIDTH = 100;
     /**
      * 连连看的每个方块的图片的高s
      */
-    public static final int PIECE_HEIGHT = 150;
+    public static final int PIECE_HEIGHT = 100;
     /**
      * 记录游戏的总事件（100秒）.
      */
